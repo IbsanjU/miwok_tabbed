d1ea53f6be1fcf547f80d32530d32e11137e5a54
ibsanju <bharath2668@gmail.com>
2020-07-06 02:41:51 +0530

    tab bar activity

---
+++ app/src/main/java/com/ibsanju/miwok/CategoryAdapter.java
@@ -0,0 +1,61 @@
+package com.ibsanju.miwok;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public
+class CategoryAdapter extends FragmentPagerAdapter {
+
+    private String tabTitles[] = new String[] { "Numbers", "Family", "Colors", "Phrases" };
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public
+    CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+    @Override
+    public
+    Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhrasesFragment();
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+    @Override
+    public
+    int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
--- app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
+++ app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
@@ -1,18 +1,18 @@
-package com.ibsanju.miwok;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-
-public
-class NumbersActivity extends AppCompatActivity {
-
-	@Override
-	protected
-	void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_category);
-
-		getSupportFragmentManager().beginTransaction().replace(R.id.container, new NumbersFragment()).commit();
-
-	}
-}
+//package com.ibsanju.miwok;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public
+//class NumbersActivity extends AppCompatActivity {
+//
+//	@Override
+//	protected
+//	void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_category);
+//
+//		getSupportFragmentManager().beginTransaction().replace(R.id.container, new NumbersFragment()).commit();
+//
+//	}
+//}
