84bfd24d65775b380f60bd8dfbbc65e8b2925a61
ibsanju <bharath2668@gmail.com>
2020-07-06 00:32:17 +0530

    Update NumbersActivity.java

--- app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
+++ app/src/main/java/com/ibsanju/miwok/NumbersActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 
 public
 class NumbersActivity extends AppCompatActivity {
-	private static final String TAG = "NumbersActivity";
+
 	@Override
 	protected
 	void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_category);
+
+		getSupportFragmentManager().beginTransaction().replace(R.id.container, new NumbersFragment()).commit();
 
 	}
 }
